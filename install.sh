13da23db8aaa196133b0d15089e058951e539969
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2019-04-19 22:24:17 +0000

    Symlink and source .bash_profile

--- install.sh
+++ install.sh
@@ -8,3 +8,10 @@ $(cd ~ && git clone https://github.com/michaeldfallen/git-radar .git-radar)
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 ln -s $(pwd)/git-completion.bash ~/.git-completion.bash
 
+# Symlink .bash_profile to home dir
+temp=$(pwd)
+ln -s $temp/.bash_profile ~/.bash_profile
+
+# Source .bash_profile during new sessions
+echo "source ~/.bash_profile" >> ~/.bashrc
+
