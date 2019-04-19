833458ae02c5ecdffcec4738667d786d05689a25
Matthew Eppelsheimer <matthew.eppelsheimer@gmail.com>
2019-04-19 22:19:49 +0000

    Include git-radar in installation

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# install git radar
+# https://github.com/michaeldfallen/git-radar
+$(cd ~ && git clone https://github.com/michaeldfallen/git-radar .git-radar)
+
 # install git bash completion
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 ln -s $(pwd)/git-completion.bash ~/.git-completion.bash
